d89e265e6259d3563dbe7153980049934fd8f6d7
teaaaaaaaa <65693349+teaaaaaaaa@users.noreply.github.com>
2022-11-07 18:42:11 -0800

    Add files via upload

--- make.bat
+++ make.bat
@@ -1,12 +1,14 @@
 call Engine\make.bat
 call Bownly\make.bat
 call Template\make.bat
+call teaaaaaaaa\make.bat
 
 C:\gbdk\bin\lcc -Wa-l -c -o Engine/o/main.o main.c
 C:\gbdk\bin\lcc -Wl-yt3 -Wl-yoA -Wl-ya4 -o GBMicroGames.gb ^
     hUGETracker/hUGEDriver.obj.o ^
     Engine/o/*.o ^
     Bownly/o/*.o Bownly/res/sprites/*.c ^
-    Template/o/*.o 
+    Template/o/*.o ^
+    teaaaaaaaa/o/*.o
+pause
 
-exit 0
