18d79a4286e67396b007ab01f68e434bd5d039cf
dovesam <9062023+dovesam@users.noreply.github.com>
2022-11-09 01:03:53 +0000

    build.sh files for unix

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+BIN=/Users/dovesam/Documents/Development/gbdk/bin
+
+source Engine/build.sh
+source Bownly/build.sh
+source Template/build.sh
+
+$BIN/lcc -Wa-l -c -o Engine/o/main.o main.c
+$BIN/lcc -Wl-yt3 -Wl-yo8 -Wl-ya4 -o GBMicroGames.gb \
+    hUGETracker/hUGEDriver.obj.o                \
+    Engine/o/*.o Engine/res/sprites/*.c         \
+    Bownly/o/*.o Bownly/res/sprites/*.c         \
+    Template/o/*.o                              
