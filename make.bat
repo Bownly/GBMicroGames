ec5a244f8b8d6726bf00405d81ab6d55fe4114d4
dovesam <9062023+dovesam@users.noreply.github.com>
2022-11-12 20:56:03 +0000

    make.bat files for Windows

--- make.bat
+++ make.bat
@@ -1,12 +1,14 @@
 call Engine\make.bat
 call Bownly\make.bat
 call Template\make.bat
+call dovesam\make.bat
 
 C:\gbdk\bin\lcc -Wa-l -c -o Engine/o/main.o main.c
 C:\gbdk\bin\lcc -Wl-yt3 -Wl-yoA -Wl-ya4 -o GBMicroGames.gb ^
     hUGETracker/hUGEDriver.obj.o ^
     Engine/o/*.o Engine/res/sprites/*.c ^
     Bownly/o/*.o Bownly/res/sprites/*.c ^
-    Template/o/*.o 
+    Template/o/*.o                      ^
+    dovesam/o/*.o dovesam/res/assets/*.c
 
 exit 0
